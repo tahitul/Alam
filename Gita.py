14b61a03035ddb0eae57e4ffb6729ad55d515c92
tahitul <44950454+tahitul@users.noreply.github.com>
2018-11-11 19:49:23 +0100

    Add files via upload
    Initial learning stage

---
+++ Gita.py
@@ -0,0 +1,14 @@
+x = "welcome to python"
+y = 1.3
+z = 2
+v= x[:11] +"the world of python"
+print(x)
+print(y+z)
+print(y-z)
+print(y*z)
+print(y/z)
+print(x[1])
+print(x[3:8])
+print(x.strip())
+print (x[:11]+"the world of python")
+print(v)
